83769a9450cde010428a53608cc90792588e8ed3
bglik <bglik@cern.ch>
2024-09-22 01:15:49 +0300

    now get an argument for minbias flag

--- jobs/qsub.sh
+++ jobs/qsub.sh
@@ -4,6 +4,6 @@ path_to_python="/usr/bin/python3"
 
 count=0
 while [ $count -le 83 ]; do
-    qsub -cwd -q citron.q@sge234 -S $path_to_python run.py $count
+    qsub -cwd -q citron.q@sge234 -S $path_to_python run.py $count $1
     count=$((count + 1))
 done
