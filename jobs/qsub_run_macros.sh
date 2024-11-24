eb2d1e198dfe1dd78a0685471296acf476945e4e
bglik <bglik@cern.ch>
2024-11-24 10:45:33 +0200

    Now run on files from EOS

--- jobs/qsub_run_macros.sh
+++ jobs/qsub_run_macros.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-qsub -cwd -q citron.q@sge234 run_macros.sh 1
-qsub -cwd -q citron.q@sge234 run_macros.sh 0
+qsub -cwd -q citron.q@sge234 -S /bin/bash wrapper_run_macros.sh 1
+qsub -cwd -q citron.q@sge234 -S /bin/bash wrapper_run_macros.sh 0
